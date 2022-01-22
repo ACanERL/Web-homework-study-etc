5bf93c633e7c6a865e716afd62816afaebbd12e6
Ahmetcan EROL <71428865+ACanERL@users.noreply.github.com>
2022-01-22 10:09:36 +0300

    Web-homework-study-etc

---
+++ JavaScript/DateTime.js
@@ -0,0 +1,3 @@
+let d=new Date();
+console.log(d.Date);
+console.log(d.getDay);
---
+++ JavaScript/calısma1.js
@@ -0,0 +1,17 @@
+var musteriAdi='Ahmet';
+var order1=100;
+var order2=200;
+var totalOder=order1+order2;
+console.log("Toplam:",totalOder);
+
+var objectDizi={
+    sehiradi:'Trabzon',
+    semt:'Ortahisar',
+    mahalle:'Boztepe',
+    daire:6
+}
+
+var hobi=['kitap okumak','bisiklet sürmek','sinema'];
+
+console.log(objectDizi);
+console.log(hobi);
